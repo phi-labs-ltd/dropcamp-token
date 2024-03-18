e9f85db02092e735f29a7f2fb463b5c004a44f0c
Drew Taylor <drew@chainofinsight.com>
2024-03-18 09:43:14 -0400

    dc token contract

---
+++ src/lib.rs
@@ -0,0 +1,128 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use cosmwasm_std::{Empty, Reply, SubMsgResult};
+use cw2::set_contract_version;
+
+pub use cw721_soulbound::{ContractError, InstantiateMsg, MintMsg, MinterResponse, QueryMsg};
+
+pub use cw721::{ContractInfoResponse};
+
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct Metadata {
+    pub airdrop: Option<String>,
+    pub description: Option<String>,
+    pub social_score: Option<u16>,
+}
+
+pub type Extension = Option<Metadata>;
+
+pub type Cw721MetadataContract<'a> = cw721_soulbound::Cw721Contract<'a, Extension, Empty, Empty, Empty>;
+
+pub type ExecuteMsg = cw721_soulbound::ExecuteMsg<Extension, Empty>;
+
+const CONTRACT_NAME: &str = "dropcamp-token";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub mod entry {
+    use super::*;
+
+    #[cfg(not(feature = "library"))]
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn instantiate(
+        deps: DepsMut,
+        _env: Env,
+        _info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> StdResult<Response> {
+        set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+        let info = ContractInfoResponse {
+            name: msg.name,
+            symbol: msg.symbol,
+        };
+        Cw721MetadataContract::default()
+            .contract_info
+            .save(deps.storage, &info)?;
+        let minter = deps.api.addr_validate(&msg.minter)?;
+        Cw721MetadataContract::default()
+            .minter
+            .save(deps.storage, &minter)?;
+        Ok(Response::default())
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn execute(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        Cw721MetadataContract::default().execute(deps, env, info, msg)
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn reply(_deps: DepsMut, _env: Env, msg: Reply) -> Result<Response, ContractError> {
+        match msg.result {
+            SubMsgResult::Ok(_) => Ok(Response::default()),
+            SubMsgResult::Err(_) => Err(ContractError::Unauthorized {}),
+        }
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn query(deps: Deps, env: Env, msg: QueryMsg<Empty>) -> StdResult<Binary> {
+        Cw721MetadataContract::default().query(deps, env, msg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cw721::{Cw721Query};
+
+    const MINTER: &str = "claim_verifier";
+
+    #[test]
+    fn use_metadata_extension() {
+        let mut deps = mock_dependencies();
+        let contract = Cw721MetadataContract::default();
+
+        let info = mock_info(MINTER, &[]);
+        let init_msg = InstantiateMsg {
+            name: "dropcamp project".to_string(),
+            symbol: "DC1".to_string(),
+            minter: MINTER.to_string(),
+        };
+        contract
+            .instantiate(deps.as_mut(), mock_env(), info.clone(), init_msg)
+            .unwrap();
+
+        let metadata_extension = Some(Metadata {
+            airdrop: Some("project name".into()),
+            description: Some("project description".into()),
+            social_score: Some(85),
+        });
+
+        let token_id = "1";
+        let mint_msg = MintMsg {
+            token_id: token_id.to_string(),
+            owner: MINTER.to_string(),
+            token_uri: None,
+            extension: metadata_extension,
+        };
+        let exec_msg = ExecuteMsg::Mint(mint_msg.clone());
+        contract
+            .execute(deps.as_mut(), mock_env(), info, exec_msg)
+            .unwrap();
+
+        let res = contract.nft_info(deps.as_ref(), token_id.into()).unwrap();
+
+        assert_eq!(res.token_uri, mint_msg.token_uri);
+        assert_eq!(res.extension, mint_msg.extension);
+    }
+}
